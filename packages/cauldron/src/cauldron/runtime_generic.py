18f2d6f7a1b1dba0c7b23fa5ecd7a3397efca0d0
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-09-05 16:49:02 +1200

    refactor: strip out some of the class scaffolding.

--- packages/cauldron/src/cauldron/runtime_generic.py
+++ packages/cauldron/src/cauldron/runtime_generic.py
@@ -1,72 +1,59 @@
 """Provides a decorator that makes a given generic class able to be instantiated with generic syntax."""
 
+from collections.abc import Callable
 from functools import cache
-from typing import TYPE_CHECKING, TypeVar, get_type_hints
+from typing import TYPE_CHECKING, get_type_hints
 
 
 class _GenericClassProcessor:
     def __init__(self, cls: type):
         self.cls = cls
 
-    @property
-    def annotations(self):
-        return get_type_hints(self.cls)
 
-    @property
-    def unbound_class_attributes(self):
-        return set(self.annotations.keys()).difference(self.__class__.__dict__.keys())
+def subclass_attributes(cls: type, types: tuple[type, ...]):
+    annotations = get_type_hints(cls)
+    unbound_class_attributes = set(annotations.keys()).difference(cls.__dict__.keys())
+    if len(unbound_class_attributes) != len(types):
+        raise TypeError(
+            f"expected {len(unbound_class_attributes)} parameter(s), got {len(types)} parameter(s)."
+        )
+    return {
+        k: dict(zip(cls.__parameters__, types, strict=True))[v.__parameters__[0]]
+        for k, v in annotations.items()
+    }
 
-    @property
-    def parameters(self) -> tuple[TypeVar]:
-        return self.cls.__parameters__
 
-    def subclass_attributes(self, types: tuple[type, ...]):
-        attributes = tuple(attr for attr in self.unbound_class_attributes)
-        if len(attributes) != len(types):
-            raise TypeError(
-                f"expected {len(attributes)} parameter(s), got {len(types)} parameter(s)."
-            )
-        return {
-            k: dict(zip(self.parameters, types, strict=True))[v.__parameters__[0]]
-            for k, v in self.annotations.items()
-        }
+def concrete_subclasser[T](cls: type[T]) -> Callable[[type | tuple[type, ...]], type]:
+    """Returns callable that modifies a class with a new __class_getitem__ method.
 
-    def concrete_subclasser(self):
-        """Returns callable that modifies a class with a new __class_getitem__ method.
+    This callable automatically returns a generated subclass
+    with the specified class attribute filled in.
+    """
 
-        This callable automatically returns a generated subclass
-        with the specified class attribute filled in.
-        """
+    def subclass(generic_type: type | tuple[type, ...]):
+        if not isinstance(generic_type, tuple):
+            generic_type = (generic_type,)
+        return type(
+            f"{cls.__name__}[{','.join(t.__name__ for t in generic_type)}]",
+            (cls,),
+            subclass_attributes(cls, types=generic_type),
+        )
 
-        def subclass(generic_type: type | tuple[type, ...]):
-            if not isinstance(generic_type, tuple):
-                generic_type = (generic_type,)
-            return type(
-                f"{self.cls.__name__}[{','.join(t.__name__ for t in generic_type)}]",
-                (self.cls,),
-                self.subclass_attributes(types=generic_type),
-            )
-
-        return subclass
-
-    def enhance(self):
-        subclass = self.concrete_subclasser()
-        current_class_getitem = getattr(self.cls, "__class_getitem__", None)
-        if (
-            current_class_getitem
-            and current_class_getitem.__name__ == subclass.__name__
-        ):
-            raise RuntimeError(
-                "Cannot decorate a class with runtime_generic more than once."
-            )
-        if not TYPE_CHECKING:
-            # The type checkers get all sorts of upset about this call
-            # But it works.
-            self.cls.__class_getitem__ = cache(subclass)
-
-        return self.cls
+    return subclass
 
 
 def runtime_generic[T](cls: type[T]) -> type[T]:
     """Decorator that makes some class's generic be able to be instantiated."""
-    return _GenericClassProcessor(cls).enhance()
+    processor = _GenericClassProcessor(cls)
+    subclasser = concrete_subclasser(cls)
+    current_class_getitem = getattr(processor.cls, "__class_getitem__", None)
+    if current_class_getitem and current_class_getitem.__name__ == subclasser.__name__:
+        raise RuntimeError(
+            "Cannot decorate a class with runtime_generic more than once."
+        )
+    if not TYPE_CHECKING:
+        # The type checkers get all sorts of upset about this call
+        # But it works.
+        processor.cls.__class_getitem__ = cache(subclasser)
+
+    return processor.cls
