ea57b6a7aecf491c66b22b657d70bcc3fee339bc
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-09-05 16:49:02 +1200

    Merge models into app for project-manager to demonstrate the minimal getting started.

--- packages/project-manager/src/project_manager/app.py
+++ packages/project-manager/src/project_manager/app.py
@@ -1,12 +1,24 @@
 from __future__ import annotations
 
+import uuid
 from functools import partial
 
 from cauldron.application import Application
 from cauldron.cli import build_cli
+from cauldron.resources.models import Resource
 from cauldron.resources.router import model_crud_router
+from sqlalchemy.orm import Mapped, mapped_column
 
-from project_manager.models import Organization
+UUID = uuid.UUID
+
+
+class Organization(Resource, kw_only=True):
+    plural_name = "organizations"
+    singular_name = "organization"
+    summary_fields = (*list(Resource.summary_fields), "name")
+    id: Mapped[UUID] = Resource.primary_foreign_key_to(init=False)
+    name: Mapped[str] = mapped_column(index=True)
+
 
 app_extra_args = {
     "title": "Project Manager Service",
