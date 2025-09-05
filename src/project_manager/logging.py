ec0969fd875356f95257b5262fd9346841836d65
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-09-05 16:49:01 +1200

    fix: logging setup.

--- src/project_manager/logging.py
+++ src/project_manager/logging.py
@@ -6,34 +6,41 @@ import structlog
 get_logger = structlog.stdlib.get_logger
 
 
+def shared_processors() -> list[Any]:
+    """returns processorts used by both strurctlog and stdlib"""
+    return [
+        structlog.stdlib.add_log_level,
+        structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S"),
+    ]
+
+
+def setup_stdlib_logging() -> None:
+    handler = logging.StreamHandler()
+    handler.setFormatter(
+        structlog.stdlib.ProcessorFormatter(
+            foreign_pre_chain=shared_processors(),
+            processors=[
+                structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+                structlog.dev.ConsoleRenderer(),
+            ],
+        )
+    )
+    root_logger = logging.getLogger()
+    root_logger.addHandler(handler)
+    root_logger.setLevel(logging.INFO)
+
+
 def setup_logging() -> None:
     """Setup structlog logging.
 
     As documented at https://www.structlog.org/en/stable/standard-library.html#rendering-using-structlog-based-formatters-within-logging
     """
-    timestamper = structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S")
-    shared_processors: list[Any] = [
-        structlog.stdlib.add_log_level,
-        timestamper,
-    ]
+    setup_stdlib_logging()
     structlog.configure(
         processors=[
-            *shared_processors,
+            *shared_processors(),
             structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
         ],
         logger_factory=structlog.stdlib.LoggerFactory(),
         cache_logger_on_first_use=True,
     )
-
-    formatter = structlog.stdlib.ProcessorFormatter(
-        foreign_pre_chain=shared_processors,
-        processors=[
-            structlog.stdlib.ProcessorFormatter.remove_processors_meta,
-            structlog.dev.ConsoleRenderer(),
-        ],
-    )
-    handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
-    root_logger = logging.getLogger()
-    root_logger.addHandler(handler)
-    root_logger.setLevel(logging.INFO)
