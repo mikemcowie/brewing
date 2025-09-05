d22b794983d3960f3cc1db63d0fb6bf8a44172fa
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-09-05 16:49:02 +1200

    refactor: move CauldronHTTP out of __init__.py

--- packages/cauldron/src/cauldron/http/__init__.py
+++ packages/cauldron/src/cauldron/http/__init__.py
@@ -1,29 +1,12 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 from fastapi import APIRouter as APIRouter
 from fastapi import Depends as Depends
-from fastapi import FastAPI
 from fastapi import Path as Path
 from fastapi import Request as Request
 from fastapi import status as status
 
+from .cauldron_http import CauldronHTTP as CauldronHTTP
 from .responses import Response as Response
 
-if TYPE_CHECKING:
-    from .viewset import AbstractViewSet
-
-__all__ = ["APIRouter", "CauldronHTTP", "Request", "Response", "status"]
-
-
-class CauldronHTTP(FastAPI):
-    def include_viewset(self, viewset: AbstractViewSet):
-        for attr in dir(viewset):
-            # This allows us to refer to a method as a depdendency
-            # during class definition time.
-            item = getattr(viewset, attr)
-            func = getattr(item, "__func__", None)
-            if callable(item) and func:
-                self.dependency_overrides[func] = item
-        self.include_router(viewset.router)
+__all__ = ["APIRouter", "CauldronHTTP", "Depends", "Request", "Response", "status"]
---
+++ packages/cauldron/src/cauldron/http/cauldron_http.py
@@ -0,0 +1,27 @@
+"""The CauldronHTTP class: the ASGI application.
+
+A subclass of FastAPI.
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from fastapi import FastAPI
+
+from .responses import Response as Response
+
+if TYPE_CHECKING:
+    from .viewset import AbstractViewSet
+
+
+class CauldronHTTP(FastAPI):
+    def include_viewset(self, viewset: AbstractViewSet):
+        for attr in dir(viewset):
+            # This allows us to refer to a method as a depdendency
+            # during class definition time.
+            item = getattr(viewset, attr)
+            func = getattr(item, "__func__", None)
+            if callable(item) and func:
+                self.dependency_overrides[func] = item
+        self.include_router(viewset.router)
