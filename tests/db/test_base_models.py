82b6143cce13d212e4d797db840697546a0e7dba
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-09-05 16:49:02 +1200

    test: more validations.

--- tests/db/test_base_models.py
+++ tests/db/test_base_models.py
@@ -1,8 +1,10 @@
 from typing import TYPE_CHECKING
+from uuid import UUID
 
+from pydantic import BaseModel, ConfigDict
 from pydantic.alias_generators import to_snake
 from sqlalchemy import MetaData
-from sqlalchemy.orm import DeclarativeBase, declared_attr
+from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr
 
 from project_manager.resources.models import (
     Resource,
@@ -27,3 +29,30 @@ def create_test_resource_cls():
 
 def test_test_resource_cls():
     assert Resource.metadata is not create_test_resource_cls().metadata
+
+
+class TestSchemaGeneration:
+    def db_model(self):
+        resource = create_test_resource_cls()
+
+        class SomeResource(resource, kw_only=True):
+            id: Mapped[UUID] = Resource.primary_foreign_key_to(init=False)
+            name: Mapped[str]
+            optional: Mapped[str | None]
+
+        return SomeResource
+
+    def test_create_model(self):
+        class CreateSomeResource(BaseModel):
+            model_config = ConfigDict(extra="forbid")
+            name: str
+            optional: str
+
+        assert str(self.db_model().schemas().create.model_fields) == str(
+            CreateSomeResource.model_fields
+        )
+        assert self.db_model().schemas().create.__name__ == CreateSomeResource.__name__
+        assert (
+            self.db_model().schemas().create.model_json_schema()
+            == CreateSomeResource.model_json_schema()
+        )
