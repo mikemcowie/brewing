f3b0c1f4c19b44fd113a38df0f2cf7795c18a170
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-09-05 16:49:02 +1200

    refactor: Make the fastapi router more generic by nesting function.

--- src/project_manager/organizations/router.py
+++ src/project_manager/organizations/router.py
@@ -8,7 +8,7 @@ from project_manager import db
 from project_manager.endpoints import Endpoints
 from project_manager.exceptions import Unauthorized
 from project_manager.organizations.models import Organization
-from project_manager.resources.models import AccessLevel, ResourceAccessItem
+from project_manager.resources.models import AccessLevel, Resource, ResourceAccessItem
 from project_manager.resources.repo import (
     CreateResource,
     CrudRepository,
@@ -19,139 +19,132 @@ from project_manager.resources.repo import (
 from project_manager.users.models import User
 from project_manager.users.router import user
 
-router = APIRouter(tags=["organizations"], dependencies=[Depends(user)])
 
+def model_crud_router[ModelT: Resource](model_type: type[ModelT]):  # noqa: C901
+    router = APIRouter(tags=["organizations"], dependencies=[Depends(user)])
 
-OrganizationRepository = CrudRepository[Organization]
+    async def repo(
+        db_session: Annotated[AsyncSession, Depends(db.db_session)],
+        user: Annotated[User, Depends(user)],
+    ):
+        return CrudRepository[model_type](db_session, user)
 
+    async def access_level(
+        repo: Annotated[CrudRepository[ModelT], Depends(repo)], organization_id: UUID
+    ) -> AccessLevel:
+        return (
+            await repo.get_access_one(resource_id=organization_id, user_id=repo.user.id)
+        ).access
 
-async def repo(
-    db_session: Annotated[AsyncSession, Depends(db.db_session)],
-    user: Annotated[User, Depends(user)],
-):
-    return OrganizationRepository(db_session, user)
+    async def access_org_owner(
+        access_level: Annotated[AccessLevel, Depends(access_level)],
+    ):
+        if access_level.is_owner():
+            return True
+        raise Unauthorized(detail="access denied")
 
+    async def access_org_contributor(
+        access_level: Annotated[AccessLevel, Depends(access_level)],
+    ):
+        if access_level.is_contributor():
+            return True
+        raise Unauthorized(detail="access denied")
 
-async def access_level(
-    repo: Annotated[OrganizationRepository, Depends(repo)], organization_id: UUID
-) -> AccessLevel:
-    return (
-        await repo.get_access_one(resource_id=organization_id, user_id=repo.user.id)
-    ).access
+    async def access_org_reader(
+        access_level: Annotated[AccessLevel, Depends(access_level)],
+    ):
+        if access_level.is_reader():
+            return True
+        raise Unauthorized(detail="access denied")
 
-
-async def access_org_owner(access_level: Annotated[AccessLevel, Depends(access_level)]):
-    if access_level.is_owner():
-        return True
-    raise Unauthorized(detail="access denied")
-
-
-async def access_org_contributor(
-    access_level: Annotated[AccessLevel, Depends(access_level)],
-):
-    if access_level.is_contributor():
-        return True
-    raise Unauthorized(detail="access denied")
-
-
-async def access_org_reader(
-    access_level: Annotated[AccessLevel, Depends(access_level)],
-):
-    if access_level.is_reader():
-        return True
-    raise Unauthorized(detail="access denied")
-
-
-@router.post(
-    Endpoints.ORGANIZATIONS,
-    status_code=status.HTTP_201_CREATED,
-    response_model=ResourceRead,
-)
-async def create_organization(
-    create: CreateResource, repo: Annotated[OrganizationRepository, Depends(repo)]
-) -> ResourceRead:
-    return await repo.create(create)
-
-
-@router.get(Endpoints.ORGANIZATIONS, response_model=list[ResourceSummary])
-async def list_organization(
-    repo: Annotated[OrganizationRepository, Depends(repo)],
-) -> list[ResourceSummary]:
-    return await repo.list_resources()
-
-
-@router.get(
-    Endpoints.ORGANIZATIONS_ONE,
-    response_model=ResourceRead,
-    dependencies=[Depends(access_org_reader)],
-)
-async def read_organization(
-    organization_id: UUID, repo: Annotated[OrganizationRepository, Depends(repo)]
-) -> ResourceRead:
-    return ResourceRead.model_validate(
-        await repo.get(organization_id), from_attributes=True
+    @router.post(
+        Endpoints.ORGANIZATIONS,
+        status_code=status.HTTP_201_CREATED,
+        response_model=ResourceRead,
     )
+    async def create_organization(
+        create: CreateResource, repo: Annotated[CrudRepository[ModelT], Depends(repo)]
+    ) -> ResourceRead:
+        return await repo.create(create)
+
+    @router.get(Endpoints.ORGANIZATIONS, response_model=list[ResourceSummary])
+    async def list_organization(
+        repo: Annotated[CrudRepository[ModelT], Depends(repo)],
+    ) -> list[ResourceSummary]:
+        return await repo.list_resources()
+
+    @router.get(
+        Endpoints.ORGANIZATIONS_ONE,
+        response_model=ResourceRead,
+        dependencies=[Depends(access_org_reader)],
+    )
+    async def read_organization(
+        organization_id: UUID, repo: Annotated[CrudRepository[ModelT], Depends(repo)]
+    ) -> ResourceRead:
+        return ResourceRead.model_validate(
+            await repo.get(organization_id), from_attributes=True
+        )
+
+    @router.put(
+        Endpoints.ORGANIZATIONS_ONE,
+        response_model=ResourceRead,
+        dependencies=[Depends(access_org_contributor)],
+    )
+    async def update_organization(
+        organization_id: UUID,
+        update: UpdateResource,
+        repo: Annotated[CrudRepository[ModelT], Depends(repo)],
+    ) -> ResourceRead:
+        return await repo.update(organization_id, update)
+
+    @router.delete(
+        Endpoints.ORGANIZATIONS_ONE,
+        status_code=status.HTTP_204_NO_CONTENT,
+        dependencies=[Depends(access_org_owner)],
+    )
+    async def delete_organization(
+        organization_id: UUID,
+        repo: Annotated[CrudRepository[ModelT], Depends(repo)],
+    ) -> None:
+        await repo.delete(organization_id)
+
+    @router.get(
+        Endpoints.ORGANIZATIONS_ONE_ACCESS,
+        response_model=list[ResourceAccessItem],
+        dependencies=[Depends(access_org_contributor)],
+    )
+    async def get_access(
+        organization_id: UUID, repo: Annotated[CrudRepository[ModelT], Depends(repo)]
+    ) -> list[ResourceAccessItem]:
+        return await repo.get_access(organization_id)
+
+    @router.get(
+        Endpoints.ORGANIZATIONS_ONE_ACCESS_ONE,
+        response_model=ResourceAccessItem,
+        dependencies=[Depends(access_org_contributor)],
+    )
+    async def get_access_one(
+        organization_id: UUID,
+        user_id: UUID,
+        repo: Annotated[CrudRepository[ModelT], Depends(repo)],
+    ) -> ResourceAccessItem:
+        return await repo.get_access_one(organization_id, user_id)
+
+    @router.post(
+        Endpoints.ORGANIZATIONS_ONE_ACCESS,
+        response_model=list[ResourceAccessItem],
+        dependencies=[Depends(access_org_owner)],
+    )
+    async def set_access(
+        organization_id: UUID,
+        access: ResourceAccessItem | list[ResourceAccessItem],
+        repo: Annotated[CrudRepository[ModelT], Depends(repo)],
+    ) -> list[ResourceAccessItem]:
+        if isinstance(access, ResourceAccessItem):
+            access = [access]
+        return await repo.set_access(organization_id, access)
+
+    return router
 
 
-@router.put(
-    Endpoints.ORGANIZATIONS_ONE,
-    response_model=ResourceRead,
-    dependencies=[Depends(access_org_contributor)],
-)
-async def update_organization(
-    organization_id: UUID,
-    update: UpdateResource,
-    repo: Annotated[OrganizationRepository, Depends(repo)],
-) -> ResourceRead:
-    return await repo.update(organization_id, update)
-
-
-@router.delete(
-    Endpoints.ORGANIZATIONS_ONE,
-    status_code=status.HTTP_204_NO_CONTENT,
-    dependencies=[Depends(access_org_owner)],
-)
-async def delete_organization(
-    organization_id: UUID,
-    repo: Annotated[OrganizationRepository, Depends(repo)],
-) -> None:
-    await repo.delete(organization_id)
-
-
-@router.get(
-    Endpoints.ORGANIZATIONS_ONE_ACCESS,
-    response_model=list[ResourceAccessItem],
-    dependencies=[Depends(access_org_contributor)],
-)
-async def get_access(
-    organization_id: UUID, repo: Annotated[OrganizationRepository, Depends(repo)]
-) -> list[ResourceAccessItem]:
-    return await repo.get_access(organization_id)
-
-
-@router.get(
-    Endpoints.ORGANIZATIONS_ONE_ACCESS_ONE,
-    response_model=ResourceAccessItem,
-    dependencies=[Depends(access_org_contributor)],
-)
-async def get_access_one(
-    organization_id: UUID,
-    user_id: UUID,
-    repo: Annotated[OrganizationRepository, Depends(repo)],
-) -> ResourceAccessItem:
-    return await repo.get_access_one(organization_id, user_id)
-
-
-@router.post(
-    Endpoints.ORGANIZATIONS_ONE_ACCESS,
-    response_model=list[ResourceAccessItem],
-    dependencies=[Depends(access_org_owner)],
-)
-async def set_access(
-    organization_id: UUID,
-    access: ResourceAccessItem | list[ResourceAccessItem],
-    repo: Annotated[OrganizationRepository, Depends(repo)],
-) -> list[ResourceAccessItem]:
-    if isinstance(access, ResourceAccessItem):
-        access = [access]
-    return await repo.set_access(organization_id, access)
+router = model_crud_router(Organization)
