26df19869370280d19338aa152fdbf6c6eeb404d
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-09-05 16:49:02 +1200

    refactor: reformat complete moving things out into repo.

--- packages/cauldron/src/cauldron/auth/models.py
+++ packages/cauldron/src/cauldron/auth/models.py
@@ -1,16 +1,10 @@
 from __future__ import annotations
 
-import base64
-import hashlib
 import uuid
-from datetime import UTC, datetime, timedelta
-from functools import cache
-from secrets import token_bytes
 from typing import TYPE_CHECKING, Literal
 
-from passlib.context import CryptContext
 from pydantic import BaseModel, EmailStr
-from sqlalchemy import DateTime, ForeignKey, select
+from sqlalchemy import DateTime, ForeignKey
 from sqlalchemy.orm import (
     Mapped,
     MappedAsDataclass,
@@ -19,24 +13,13 @@ from sqlalchemy.orm import (
     relationship,
 )
 
-from cauldron.auth.exceptions import InvalidToken
 from cauldron.db.base import Base
 from cauldron.db.columns import created_field, updated_field, uuid_primary_key
-from cauldron.exceptions import Unauthorized
 
 if TYPE_CHECKING:
-    from sqlalchemy.ext.asyncio import AsyncSession
-
-ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 12
-TOKEN_BYTES = 48
-
+    from datetime import datetime
 
 UUID = uuid.UUID
-
-
-@cache
-def password_context() -> CryptContext:
-    return CryptContext(schemes=["argon2"], deprecated="auto")
 
 
 class User(MappedAsDataclass, Base, kw_only=True):
@@ -47,11 +30,6 @@ class User(MappedAsDataclass, Base, kw_only=True):
     created: Mapped[datetime] = created_field()
     updated: Mapped[datetime] = created_field()
 
-    def __post_init__(self):
-        if self.password:
-            self.password_hash = password_context().hash(self.password)
-            self.password = ""  # forget the actual password.
-
     @staticmethod
     def primary_foreign_key_to(*, init: bool) -> MappedColumn[UUID]:
         return mapped_column(ForeignKey("user.id"), primary_key=True, init=init)
@@ -60,39 +38,6 @@ class User(MappedAsDataclass, Base, kw_only=True):
     def username(self) -> str:
         """Username is an alias for email."""
         return self.email
-
-    @classmethod
-    async def authorize(cls, session: AsyncSession, email: str, password: str) -> Token:
-        user = (
-            await session.execute(select(cls).where(cls.email == email))
-        ).scalar_one_or_none()
-        if (
-            user is None
-            or not user.password_hash
-            or not password_context().verify(password, user.password_hash)
-        ):
-            raise Unauthorized("incorrect username or password")
-        # password has been validated, so from here we are just constrcting a user session,
-        # flushing to the database, and returning the token
-        token = base64.b64encode(token_bytes(TOKEN_BYTES)).decode()
-        user_session = UserSession(
-            id=hashlib.sha512(token.encode()).hexdigest(),
-            user=user,
-            expires=datetime.now(UTC) + timedelta(ACCESS_TOKEN_EXPIRE_MINUTES),
-        )
-        session.add(user_session)
-        session.add(user)
-        await session.flush()
-        await session.commit()
-        return Token(access_token=token)
-
-    @classmethod
-    async def create_user(cls, session: AsyncSession, new_user: UserRegister) -> User:
-        async with session.begin():
-            user = User(**new_user.model_dump())
-            session.add(user)
-            await session.flush()
-            return user
 
 
 class UserSession(MappedAsDataclass, Base, kw_only=True):
@@ -105,29 +50,6 @@ class UserSession(MappedAsDataclass, Base, kw_only=True):
 
     def __post_init__(self):
         self.encrypted_token = b""
-
-    @classmethod
-    async def validated(cls, token: str, session: AsyncSession) -> UserSession | None:
-        user_session = (
-            await session.execute(
-                select(cls)
-                .where(cls.id == hashlib.sha512(token.encode()).hexdigest())
-                .where(cls.expires > datetime.now(UTC))
-            )
-        ).scalar_one_or_none()
-        if not user_session:
-            return None
-        return user_session
-
-    @classmethod
-    async def authenticated_user(
-        cls, session: AsyncSession, token: str | None
-    ) -> User | None:
-        if not token:
-            return None
-        if user_session := await cls.validated(token, session):
-            return user_session.user
-        raise InvalidToken(detail="invalid token")
 
 
 class Token(BaseModel):
--- packages/cauldron/src/cauldron/auth/users.py
+++ packages/cauldron/src/cauldron/auth/users.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 
+import base64
 import hashlib
 from dataclasses import dataclass
-from datetime import UTC, datetime
+from datetime import UTC, datetime, timedelta
 from functools import cached_property
+from secrets import token_bytes
 from typing import TYPE_CHECKING, Annotated
 
+from passlib.context import CryptContext
 from runtime_generic import runtime_generic
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -15,12 +18,17 @@ from cauldron.auth.models import Token, User, UserRead, UserRegister, UserSessio
 from cauldron.db.session import (
     db_session,
 )
+from cauldron.exceptions import Unauthorized
 from cauldron.http import APIRouter, Depends, Request, status
 from cauldron.http.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 if TYPE_CHECKING:
     from pydantic import SecretStr
     from sqlalchemy.ext.asyncio import AsyncSession
+
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 12
+TOKEN_BYTES = 48
 
 
 def secret_value(value: str | SecretStr) -> str:
@@ -30,6 +38,7 @@ def secret_value(value: str | SecretStr) -> str:
 class UserRepo:
     def __init__(self, session: AsyncSession):
         self._session = session
+        self._password_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
     async def validated(self, token: str) -> UserSession | None:
         user_session = (
@@ -49,6 +58,39 @@ class UserRepo:
         if user_session := await self.validated(token):
             return user_session.user
         raise InvalidToken(detail="invalid token")
+
+    async def authorize(self, username: str, password: str):
+        user = (
+            await self._session.execute(select(User).where(User.email == username))
+        ).scalar_one_or_none()
+        if (
+            user is None
+            or not user.password_hash
+            or not self._password_context.verify(password, user.password_hash)
+        ):
+            raise Unauthorized("incorrect username or password")
+        # password has been validated, so from here we are just constrcting a user session,
+        # flushing to the database, and returning the token
+        token = base64.b64encode(token_bytes(TOKEN_BYTES)).decode()
+        user_session = UserSession(
+            id=hashlib.sha512(token.encode()).hexdigest(),
+            user=user,
+            expires=datetime.now(UTC) + timedelta(ACCESS_TOKEN_EXPIRE_MINUTES),
+        )
+        self._session.add(user_session)
+        self._session.add(user)
+        await self._session.flush()
+        await self._session.commit()
+        return Token(access_token=token)
+
+    async def create_user(self, new_user: UserRegister) -> User:
+        async with self._session.begin():
+            user = User(**new_user.model_dump())
+            user.password_hash = self._password_context.hash(user.password)
+            user.password = ""  # forget the actual password.
+            self._session.add(user)
+            await self._session.flush()
+            return user
 
 
 @dataclass
@@ -80,6 +122,16 @@ class UserService[RepoT: UserRepo, AuthConfigT: AuthConfig]:
             raise InvalidToken("unauthorized")
         return user
 
+    async def authorize(self, form: OAuth2PasswordRequestForm):
+        return await self._repo.authorize(
+            username=form.username, password=secret_value(form.password)
+        )
+
+    async def register(self, user: UserRegister):
+        return UserRead.model_validate(
+            await self._repo.create_user(user), from_attributes=True
+        )
+
 
 async def service(db_session: Annotated[AsyncSession, Depends(db_session)]):
     return UserService[UserRepo, AuthConfig](session=db_session)
@@ -103,18 +155,14 @@ async def user_own_profile(user: Annotated[User, Depends(user)]) -> User:
 @router.post("/users/login")
 async def login(
     form: Annotated[OAuth2PasswordRequestForm, Depends()],
-    db_session: Annotated[AsyncSession, Depends(db_session)],
+    service: Annotated[UserService[UserRepo, AuthConfig], Depends(service)],
 ) -> Token:
-    return await User.authorize(
-        db_session, email=form.username, password=secret_value(form.password)
-    )
+    return await service.authorize(form)
 
 
 @router.post("/users/register", status_code=status.HTTP_201_CREATED)
 async def register(
     user: UserRegister,
-    db_session: Annotated[AsyncSession, Depends(db_session)],
+    service: Annotated[UserService[UserRepo, AuthConfig], Depends(service)],
 ) -> UserRead:
-    return UserRead.model_validate(
-        await User.create_user(db_session, user), from_attributes=True
-    )
+    return await service.register(user)
