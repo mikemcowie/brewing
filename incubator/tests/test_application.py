25ba791abfc70e1904d62b4b7cff559aee78954f
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-09-06 15:14:59 +1200

    test: add test covering difference between dev and prod app.

---
+++ incubator/tests/test_application.py
@@ -0,0 +1,24 @@
+from unittest.mock import MagicMock
+
+from cauldron_incubator.application import Application
+from cauldron_incubator.configuration import BaseConfiguration
+from starlette.routing import Mount
+
+
+def new_config_cls():
+    class Config(BaseConfiguration):
+        cli_provider = MagicMock()
+        title = "Test"
+        description = "Test Config"
+        version = "1"
+
+    return Config
+
+
+def test_dev_api_extra_mounts():
+    config = new_config_cls()
+    app = Application[config](viewsets=[])
+    dev_mount_paths = [p.path for p in app.dev_app.routes if isinstance(p, Mount)]
+    prod_mount_paths = [p.path for p in app.app.routes if isinstance(p, Mount)]
+    assert not prod_mount_paths
+    assert dev_mount_paths == ["/htmlcov", "/testreport"]
