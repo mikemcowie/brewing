bf50ed74b1f4adec65a5b29ec68b6f8bf3dfa4d1
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-09-06 16:49:14 +1200

    test: add unit tests for service.

---
+++ incubator/tests/auth/test_service.py
@@ -0,0 +1,64 @@
+from types import SimpleNamespace
+from unittest.mock import AsyncMock, Mock
+
+import pytest
+from cauldron_incubator.auth.exceptions import InvalidToken
+from cauldron_incubator.auth.users import UserService
+from cauldron_incubator.exceptions import Unauthorized
+from fastapi.requests import Request
+
+
+@pytest.mark.asyncio
+async def test_user_from_request():
+    user = object()
+
+    class FakeRepo:
+        def __init__(self, *_, **__):  # type: ignore
+            pass
+
+        async def validated(self, token: str):  # noqa
+            return SimpleNamespace(user=user)
+
+    svc = UserService[FakeRepo, Mock](AsyncMock())  # type: ignore
+    svc.token = AsyncMock()
+    svc.token.return_value = "TOKEN"
+
+    assert await svc.user_from_request(Request({"type": "http"})) is user
+
+
+@pytest.mark.asyncio
+async def test_user_from_request_if_none_returned_by_repo():
+    user = object()
+
+    class FakeRepo:
+        def __init__(self, *_, **__):  # type: ignore
+            pass
+
+        async def validated(self, token: str):  # noqa: ARG002
+            return None
+
+    svc = UserService[FakeRepo, Mock](AsyncMock())  # type: ignore
+    svc.token = AsyncMock()
+    svc.token.return_value = "TOKEN"
+
+    with pytest.raises(InvalidToken):
+        assert await svc.user_from_request(Request({"type": "http"})) is user
+
+
+@pytest.mark.asyncio
+async def test_user_from_request_if_no_token_in_request():
+    user = object()
+
+    class FakeRepo:
+        def __init__(self, *_, **__):  # type: ignore
+            pass
+
+        async def validated(self, token: str):  # noqa: ARG002
+            return SimpleNamespace(user=user)
+
+    svc = UserService[FakeRepo, Mock](AsyncMock())  # type: ignore
+    svc.token = AsyncMock()
+    svc.token.return_value = None
+
+    with pytest.raises(Unauthorized):
+        assert await svc.user_from_request(Request({"type": "http"})) is user
