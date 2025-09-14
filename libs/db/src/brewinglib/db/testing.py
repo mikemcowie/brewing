aa85b52f987ff8c4273ca9001765df965b23d990
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-09-14 16:55:03 +1200

    feat: further layout of basic test and type params for db.

--- libs/db/src/brewinglib/db/testing.py
+++ libs/db/src/brewinglib/db/testing.py
@@ -1,56 +1,93 @@
 from __future__ import annotations
 
-from abc import abstractmethod
-from typing import TYPE_CHECKING, ClassVar
+import os
+from collections.abc import Callable, Generator, MutableMapping
+from contextlib import AbstractContextManager, contextmanager
+from functools import partial
 
 from brewinglib.db.types import DatabaseType
+from testcontainers.mysql import MySqlContainer
+from testcontainers.postgres import PostgresContainer
 
-if TYPE_CHECKING:
-    from types import TracebackType
+
+@contextmanager
+def env(
+    new_env: dict[str, str], environ: MutableMapping[str, str] = os.environ
+) -> Generator[None]:
+    """Temporarily modify environment (or other provided mapping), restore original values on cleanup."""
+    orig: dict[str, str | None] = {}
+    for key, value in new_env.items():
+        orig[key] = environ.get(key)
+        environ[key] = value
+    yield
+    # Cleanup - restore the original values
+    # or delete if they weren't set.
+    for key, value in orig.items():
+        if value is None:
+            del environ[key]
+        else:
+            environ[key] = value
 
 
-class TestingDatabase:
-    db_type: DatabaseType
-    implementations: ClassVar[dict[DatabaseType, type[TestingDatabase]]] = {}
+type TestingDatabase = Callable[[], AbstractContextManager[None]]
 
-    def __init_subclass__(cls, db_type: DatabaseType) -> None:
-        cls.db_type = db_type
-        if implementation := TestingDatabase.implementations.get(db_type):
-            raise RuntimeError(
-                f"Cannot register test database class for {db_type=}; "
-                f"{implementation=} is already registered"
-            )
-        TestingDatabase.implementations[db_type] = cls
 
-    @abstractmethod
-    def __init__(self):
-        """Initialize the testingdatabase class."""
-
-    @abstractmethod
-    def __enter__(self):
-        """Configure the database"""
-
-    @abstractmethod
-    def __exit__(
-        self,
-        exc_type: type[BaseException] | None,
-        exc_val: BaseException | None,
-        exc_tb: TracebackType | None,
+@contextmanager
+def postgresql():
+    with (
+        PostgresContainer() as pg,
+        env(
+            {
+                "PGHOST": "127.0.0.1",
+                "PGPORT": str(pg.port),
+                "PGDATABASE": pg.dbname,
+                "PGUSER": pg.username,
+                "PGPASSWORD": pg.password,
+            }
+        ),
     ):
-        """cleanup the database"""
+        yield
 
 
-class TestingSQLite(TestingDatabase, db_type=DatabaseType.sqlite):
-    pass
+@contextmanager
+def sqlite():
+    with env({"SQLITE_DATABASE": ":memory:"}):
+        yield
 
 
-class TestingPostgresql(TestingDatabase, db_type=DatabaseType.postgresql):
-    pass
+@contextmanager
+def mysql(image: str = "mysql:latest"):
+    with (
+        MySqlContainer(image=image) as mysql,
+        env(
+            {
+                "MYSQL_HOST": "127.0.0.1",
+                "MYSQL_USER": mysql.username,
+                "MYSQL_PWD": mysql.password,
+                "MYSQL_TCP_PORT": str(mysql.port),
+                "MYSQL_DATABASE": mysql.dbname,
+            }
+        ),
+    ):
+        yield
 
 
-class TestingMySQL(TestingDatabase, db_type=DatabaseType.mysql):
-    pass
+mariadb = partial(mysql, image="mariadb:latest")
 
 
-class TestingMariaDB(TestingDatabase, db_type=DatabaseType.mariadb):
-    pass
+_TEST_DATABASE_IMPLEMENTATIONS: dict[DatabaseType, TestingDatabase] = {
+    DatabaseType.sqlite: sqlite,
+    DatabaseType.postgresql: postgresql,
+    DatabaseType.mysql: mysql,
+    DatabaseType.mariadb: mariadb,
+}
+
+
+@contextmanager
+def testing(db_type: DatabaseType):
+    with _TEST_DATABASE_IMPLEMENTATIONS[db_type]():
+        yield
+
+
+# make sure pytest doesn't try this
+testing.__test__ = False  # type: ignore
--- libs/db/tests/test_types.py
+++ libs/db/tests/test_types.py
@@ -1,7 +1,5 @@
-import pytest
 from brewinglib.db import types
 
 
-@pytest.mark.parametrize("db_type", types.DatabaseType)
 def test_db_types_to_dialects_is_exhaustive(db_type: types.DatabaseType):
     assert isinstance(db_type.dialect(), types.Dialect)
