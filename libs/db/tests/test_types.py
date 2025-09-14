f35b5b9cadbc5bbb8ab8c2d86441601926df5c60
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-09-14 17:42:14 +1200

    refactor: reorganize around the types module.

--- libs/db/tests/test_types.py
+++ libs/db/tests/test_types.py
@@ -1,5 +1,6 @@
-from brewinglib.db import types
+from brewinglib.db import settings
+from brewinglib.db.settings import DatabaseType
 
 
-def test_db_types_to_dialects_is_exhaustive(db_type: types.DatabaseType):
-    assert isinstance(db_type.dialect(), types.Dialect)
+def test_db_types_to_dialects_is_exhaustive(db_type: DatabaseType):
+    assert isinstance(db_type.dialect(), settings.Dialect)
