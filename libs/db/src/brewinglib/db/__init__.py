969e03406c202bebc0255940ff10e0cad0c24c07
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-09-14 17:32:20 +1200

    refactor: move the protocol into the types module.

--- libs/db/src/brewinglib/db/__init__.py
+++ libs/db/src/brewinglib/db/__init__.py
@@ -1,1 +1,5 @@
 """Database helper package."""
+
+from .database import Database as Database
+
+__all__ = ["Database"]
--- libs/db/tests/conftest.py
+++ libs/db/tests/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from brewinglib.db import testing, types
+from brewinglib.db import Database, testing, types
 
 
 @pytest.fixture(scope="session", params=types.DatabaseType)
@@ -12,3 +12,8 @@ def db_type(request: pytest.FixtureRequest):
 def running_db(db_type: types.DatabaseType):
     with testing.testing(db_type):
         yield
+
+
+@pytest.fixture
+def database(db_type: types.DatabaseType, running_db: None):
+    return Database[db_type.dialect().connection_config_type]()
