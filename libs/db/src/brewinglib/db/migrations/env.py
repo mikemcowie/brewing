d6fcdc9ee8dd653a69ab20e332d7d5ceffeb2a27
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-09-20 00:12:00 +1200

    initial alembic generation.

---
+++ libs/db/src/brewinglib/db/migrations/env.py
@@ -0,0 +1,23 @@
+from alembic import context
+
+
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+    # late import to avoid circular import
+
+    with Database[PostgresqlSettings]().sync_engine.connect() as connection:
+        context.configure(connection=connection, target_metadata=Base.metadata)
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():  # pragma: no cover
+    raise NotImplementedError()
+else:
+    run_migrations_online()
