87fb5db673c4513a824aaec4ab20df122f5b0355
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-09-20 15:54:49 +1200

    feat: add CLI as base class for db.

--- libs/db/tests/conftest.py
+++ libs/db/tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
+import pytest_asyncio
 from brewinglib.db import Database, settings, testing
+from sqlalchemy import MetaData
 from testing_samples import db_sample1
 
 
@@ -10,9 +12,23 @@ def db_type(request: pytest.FixtureRequest):
 
 
 @pytest.fixture(scope="session")
-def running_db(db_type: settings.DatabaseType):
+def running_db_session(db_type: settings.DatabaseType):
     with testing.testing(db_type):
         yield
+
+
+@pytest_asyncio.fixture()
+async def refresh_engine(db_type: settings.DatabaseType, running_db_session: None):
+    engine = Database[db_type.dialect().connection_config_type](MetaData()).engine
+    # "Disposing" the engine here helps avoid the dreaded "attached to a different event loop"
+    # As, within the migrations machinery, we do dip in and out of new event loops, and we
+    # can't maintain a common connection pool through this.
+    await engine.dispose()
+
+
+@pytest.fixture
+def running_db(running_db_session: None, refresh_engine: None):
+    return
 
 
 @pytest.fixture
