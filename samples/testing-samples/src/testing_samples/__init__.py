2f3e7958710d8a06b3dfb88fcf6022b01ecfe4a5
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-09-20 14:51:55 +1200

    test: minor cleanups of testing.

--- samples/testing-samples/src/testing_samples/__init__.py
+++ samples/testing-samples/src/testing_samples/__init__.py
@@ -1,2 +1,8 @@
-def hello() -> str:
-    return "Hello from testing-samples!"
+"""An internal package to serve test suites.
+
+By packaging various samples here:
+
+1.imports are easier; test machinery is harder to import correctly in a monorepo
+  if it isn't package properly.
+2. decent chance samples can be reused/repurposed for different bits of brewing.
+"""
