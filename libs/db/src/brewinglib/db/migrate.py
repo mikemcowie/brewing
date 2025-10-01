8955633a197a527b63e6cc68dcb8a8fec154eb3d
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-10-01 20:55:22 +1300

    refactor: move the env dir to a path that clarifies its privateness; use config rather than runner as the contextvar.

--- libs/db/src/brewinglib/db/migrate.py
+++ libs/db/src/brewinglib/db/migrate.py
@@ -17,8 +17,18 @@ if TYPE_CHECKING:
     from brewinglib.db.types import DatabaseProtocol
     from sqlalchemy.engine import Connection
 
-type MigrationsDir = Path
+MIGRATIONS_CONTEXT_DIRECTORY = Path(__file__).parent / "_migrationcontext"
 type RevisionsDir = Path
+
+
+### Begin configuration machery for alembic context
+# This is basically working around the challenge of being hard to otherwise
+# call the machinery in env.py with parameters.
+# instead we use a contextvar, and `set_runner` contextmanager to set the value
+# before alembic gets invoked, removing it after.
+
+# configvar itself is private, as the 2 functions below it are the interface to it.
+_current_config: ContextVar[MigrationsConfig] = ContextVar("current_config")
 
 
 class MigrationsConfigError(RuntimeError):
@@ -28,8 +38,11 @@ class MigrationsConfigError(RuntimeError):
 @dataclass(frozen=True, kw_only=True)
 class MigrationsConfig:
     database: DatabaseProtocol
-    migrations_dir: MigrationsDir = Path(__file__).parent / "migrations"
     revisions_dir: RevisionsDir
+
+    @cached_property
+    def runner(self):
+        return MigrationRunner(self)
 
     @property
     def engine(self):
@@ -42,44 +55,30 @@ class MigrationsConfig:
     @cached_property
     def alembic(self) -> AlembicConfig:
         config = AlembicConfig()
-        if not self.migrations_dir.is_dir():
-            raise MigrationsConfigError(
-                f"path {self.migrations_dir!s} is not a valid directory."
-            )
         if not self.revisions_dir.is_dir():
             raise MigrationsConfigError(
                 f"path {self.revisions_dir!s} is not a valid directory."
             )
-        config.set_main_option("script_location", str(self.migrations_dir))
+        config.set_main_option("script_location", str(MIGRATIONS_CONTEXT_DIRECTORY))
         config.set_main_option("version_locations", str(self.revisions_dir))
         config.set_main_option("path_separator", ";")
         config.set_main_option("file_template", "rev_%%(rev)s_%%(slug)s")
         return config
 
 
-### Begin configuration machery for alembic context
-# This is basically working around the challenge of being hard to otherwise
-# call the machinery in env.py with parameters.
-# instead we use a contextvar, and `set_runner` contextmanager to set the value
-# before alembic gets invoked, removing it after.
-
-# configvar itself is private, as the 2 functions below it are the interface to it.
-_current_runner: ContextVar[MigrationRunner] = ContextVar("current_config")
-
-
 @contextlib.contextmanager
-def set_runner(runner: MigrationRunner):
-    token = _current_runner.set(runner)
+def set_config(config: MigrationsConfig):
+    token = _current_config.set(config)
     yield
-    _current_runner.reset(token)
+    _current_config.reset(token)
 
 
 _NO_DEFAULT = object()
 
 
-def current_runner[DefaultT](
+def current_config[DefaultT](
     default: DefaultT = _NO_DEFAULT,
-) -> DefaultT | MigrationRunner:
+) -> DefaultT | MigrationsConfig:
     """Load the current migrations config.
 
     A default value can be passed, which if set, will be returned if
@@ -87,7 +86,7 @@ def current_runner[DefaultT](
     currently set.
     """
     try:
-        return _current_runner.get()
+        return _current_config.get()
     except LookupError:
         if default is _NO_DEFAULT:
             raise
@@ -103,7 +102,7 @@ class Migrations:
     ):
         self._config = config
         self._runner = MigrationRunner(config)
-        self.set_runner = partial(set_runner, self._runner)
+        self.set_config = partial(set_config, self._config)
 
     @property
     def config(self):
@@ -116,7 +115,7 @@ class Migrations:
 
         with (
             testing.testing(self.config.database.database_type),
-            set_runner(self._runner),
+            set_config(self._config),
         ):
             command.revision(
                 self._config.alembic,
@@ -127,27 +126,27 @@ class Migrations:
 
     def upgrade(self, revision: str = "head"):
         """Upgrade the database"""
-        with set_runner(self._runner):
+        with set_config(self._config):
             command.upgrade(self._config.alembic, revision=revision)
 
     def downgrade(self, revision: str):
         """Downgrade the database"""
-        with set_runner(self._runner):
+        with set_config(self._config):
             command.downgrade(self._config.alembic, revision=revision)
 
     def stamp(self, revision: str):
         """Write to the versions table as if the database is set to the given revision."""
-        with set_runner(self._runner):
+        with set_config(self._config):
             command.stamp(self._config.alembic, revision=revision)
 
     def current(self, verbose: bool = False):
         """Display the current revision."""
-        with set_runner(self._runner):
+        with set_config(self._config):
             command.current(self._config.alembic, verbose=verbose)
 
     def check(self):
         """Validate that the database is updated to the latest revision."""
-        with set_runner(self._runner):
+        with set_config(self._config):
             command.check(self._config.alembic)
 
 
@@ -187,10 +186,10 @@ class MigrationRunner:
 
 
 def run():
-    if runner := current_runner(default=None):
+    if config := current_config(default=None):
         if context.is_offline_mode():
-            runner.run_migrations_offline()
+            config.runner.run_migrations_offline()
         else:
-            runner.run_migrations_online()
+            config.runner.run_migrations_online()
     else:
         raise RuntimeError("no current runner configured.")
