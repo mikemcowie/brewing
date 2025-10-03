a481692aa6d951a5a452e2e3185b2ce58ee79ba1
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-10-03 20:29:51 +1300

    feat: initial take at acceptance tests for functional endpoints.

--- framework/src/brewing/http/viewset_test.py
+++ framework/src/brewing/http/viewset_test.py
@@ -1,8 +1,8 @@
 """Unit tests for the viewset class."""
 
 from typing import Annotated
-from . import ViewSet, BrewingHTTP, status
-from .testing import TestClient
+from brewing.http import ViewSet, BrewingHTTP, status
+from brewing.http.testing import TestClient
 from fastapi import APIRouter, Header, Depends, Query
 from pydantic import BaseModel
 
@@ -35,7 +35,7 @@ def test_viewset_router_if_provided():
 vs1 = ViewSet()
 
 
-class ReturningData(BaseModel):
+class SomeData(BaseModel):
     """Basic response data structure."""
 
     something: list[int]
@@ -47,12 +47,12 @@ def dependency(data: Annotated[str | None, Header()] = None):
     return data
 
 
-@vs1.get("/", response_model=ReturningData)
+@vs1.get("/", response_model=SomeData)
 def endpoint1(
     data: Annotated[str | None, Depends(dependency)], count: Annotated[int, Query()] = 0
 ):
     """Return some data."""
-    return ReturningData(something=[n for n in range(count)], data=data)
+    return SomeData(something=[n for n in range(count)], data=data)
 
 
 def test_functional_fastapi_style_endpoint():
@@ -67,3 +67,165 @@ def test_functional_fastapi_style_endpoint():
     response = client.get("/", headers={"data": "foo"})
     assert response.status_code == status.HTTP_200_OK
     assert response.json()["data"] == "foo"
+
+
+"""Functional viewset usage - still similar pattern to fastapi.
+
+The upper-case method names in the decorators are used for the brewing-specific variants.
+"""
+vs2 = ViewSet()
+
+
+@vs2.GET("/", response_model=SomeData)
+def vs2_new_decorator_with_fastapi_style(
+    data: Annotated[str | None, Depends(dependency)], count: Annotated[int, Query()] = 0
+):
+    """Fastapi style  ."""
+    return SomeData(something=[n for n in range(count)], data=data)
+
+
+def test_new_decorator_with_fastapi_style_endpoint():
+    """Test that the above declared fastapi style endpoint works as per fastapi."""
+    client = new_client(vs2)
+    response = client.get("/")
+    assert response.status_code == status.HTTP_200_OK, response.text
+    assert response.json()["something"] == []
+    response = client.get("/?count=2")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["something"] == [0, 1]
+    response = client.get("/", headers={"data": "foo"})
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["data"] == "foo"
+
+
+vs3 = ViewSet()
+
+
+@vs3.GET
+def vs3_new_decorator_simplified_decorator(
+    data: Annotated[str | None, Depends(dependency)], count: Annotated[int, Query()] = 0
+):
+    """The method itself is a valid decorator, corresping to the root of the viewset."""
+    return SomeData(something=[n for n in range(count)], data=data)
+
+
+def test_new_decorator_simplified_usage():
+    """Test that the above declared fastapi style endpoint works as per fastapi."""
+    client = new_client(vs3)
+    response = client.get("/")
+    assert response.status_code == status.HTTP_200_OK, response.text
+    assert response.json()["something"] == []
+    response = client.get("/?count=2")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["something"] == [0, 1]
+    response = client.get("/", headers={"data": "foo"})
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["data"] == "foo"
+
+
+vs4 = ViewSet()
+items = vs4("items")
+item_id = items("{item_id}")
+
+
+@items.GET
+def vs4_with_new_path_style(
+    data: Annotated[str | None, Depends(dependency)], count: Annotated[int, Query()] = 0
+):
+    """The method itself is a valid decorator, corresping to the root of the viewset."""
+    return SomeData(something=[n for n in range(count)], data=data)
+
+
+def test_new_decorator_new_pathing():
+    """Test that the above declared fastapi style endpoint works as per fastapi."""
+    client = new_client(vs3)
+    response = client.get("/items/")
+    assert response.status_code == status.HTTP_200_OK, response.text
+    assert response.json()["something"] == []
+    response = client.get("/items/?count=2")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["something"] == [0, 1]
+    response = client.get("/items/", headers={"data": "foo"})
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["data"] == "foo"
+
+
+@item_id.GET
+def vs4_with_path_param(item_id: int):
+    """Return an item of type int."""
+    return {"type": "item", "id": item_id}
+
+
+def test_new_decorator_new_pathing():
+    """Test that the above declared fastapi style endpoint works as per fastapi."""
+    client = new_client(vs3)
+    response = client.get("/items/1")
+    assert response.status_code == status.HTTP_200_OK, response.text
+    assert response.json() == {"type": "item", "id": 1}
+
+
+"""In addition to standard HTTP methods, a  DEPENDS can be declared which functions as a prerequisite for all HTTP methods, and all nested endpoints."""
+
+vs5 = ViewSet()
+items = vs4("items")
+item_id = items("{item_id}")
+
+
+@items.DEPENDS
+def must_provide_header(request: Request):
+    if value := request.headers.get("required-header"):
+        return value
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED, detail="nope, can't do that."
+    )
+
+
+@items.GET
+def get_items() -> list[SomeData]:
+    return []
+
+
+@items.POST
+def create_item(data: SomeData) -> SomeData:
+    return data
+
+
+@item_id.GET
+def get_item_by_id(
+    item_id: int, value: Annotated[str, Depends(must_provide_header)]
+) -> SomeData:
+    return SomeData(something=[item_id], data=value)
+
+
+def test_depends_blocking_path():
+    # Dependency blocks requests that don't get through the dependency
+    client = new_client(vs3)
+    response = client.get("/items/")
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json() == {"detail": "nope, can't do that."}
+    response = client.post(
+        "/items/", json=SomeData(something=[], data="").model_dump(mode="json")
+    )
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json() == {"detail": "nope, can't do that."}
+    response = client.get("/items/1")
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json() == {"detail": "nope, can't do that."}
+
+
+def test_depends_passing_path():
+    """If header is set, these endpoints give 200"""
+    client = new_client(vs3)
+    client.headers["required-header"] = "somevalue"
+    response = client.get("/items/")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == []
+    response = client.post(
+        "/items/", json=SomeData(something=[], data="").model_dump(mode="json")
+    )
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"something": [], "data": ""}
+    response = client.get("/items/1")
+    # And the fastapi-style dependency in this endpoint works too.
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"something": [1], "data": "somevalue"}
