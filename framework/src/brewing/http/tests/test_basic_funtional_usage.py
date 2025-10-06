0510c66388c4ac58d386530509971f856844af70
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-10-06 22:53:14 +1300

    refactor: split up the fairly large viewset tests.

---
+++ framework/src/brewing/http/tests/test_basic_funtional_usage.py
@@ -0,0 +1,97 @@
+"""Functional viewset usage - the path component is removed"""
+
+from typing import Annotated
+from brewing.http import ViewSet, status
+from .helpers import SomeData, new_client, dependency
+from fastapi import Depends, Query
+
+
+vs2 = ViewSet()
+
+
+@vs2.GET(response_model=SomeData)
+def vs2_new_decorator_with_fastapi_style(
+    data: Annotated[str | None, Depends(dependency)], count: Annotated[int, Query()] = 0
+):
+    """Fastapi style  ."""
+    return SomeData(something=[n for n in range(count)], data=data)
+
+
+def test_new_decorator_with_fastapi_style_endpoint():
+    """Test that the above declared fastapi style endpoint works as per fastapi."""
+    client = new_client(vs2)
+    response = client.get("/")
+    assert response.status_code == status.HTTP_200_OK, response.text
+    assert response.json()["something"] == []
+    response = client.get("/?count=2")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["something"] == [0, 1]
+    response = client.get("/", headers={"data": "foo"})
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["data"] == "foo"
+
+
+vs3 = ViewSet()
+
+
+@vs3.GET()
+def vs3_new_decorator_simplified_decorator(
+    data: Annotated[str | None, Depends(dependency)], count: Annotated[int, Query()] = 0
+):
+    """The method itself is a valid decorator, corresping to the root of the viewset."""
+    return SomeData(something=[n for n in range(count)], data=data)
+
+
+def test_new_decorator_simplified_usage():
+    """Test that the above declared fastapi style endpoint works as per fastapi."""
+    client = new_client(vs3)
+    response = client.get("/")
+    assert response.status_code == status.HTTP_200_OK, response.text
+    assert response.json()["something"] == []
+    response = client.get("/?count=2")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["something"] == [0, 1]
+    response = client.get("/", headers={"data": "foo"})
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["data"] == "foo"
+
+
+vs4 = ViewSet()
+items = vs4("items")
+item_id = items("{item_id}")
+
+
+@items.GET()
+def vs4_with_new_path_style(
+    data: Annotated[str | None, Depends(dependency)], count: Annotated[int, Query()] = 0
+):
+    """The method itself is a valid decorator, corresping to the root of the viewset."""
+    return SomeData(something=[n for n in range(count)], data=data)
+
+
+def test_new_decorator_new_pathing():
+    """Test that the above declared fastapi style endpoint works as per fastapi."""
+    client = new_client(vs4)
+    response = client.get("/items/")
+    assert response.status_code == status.HTTP_200_OK, response.text
+    assert response.json()["something"] == []
+    response = client.get("/items/?count=2")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["something"] == [0, 1]
+    response = client.get("/items/", headers={"data": "foo"})
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["data"] == "foo"
+
+
+@item_id.GET()
+def vs4_with_path_param(item_id: int) -> dict[str, str | int]:
+    """Return an item of type int."""
+    return {"type": "item", "id": item_id}
+
+
+def test_new_decorator_new_pathing_with_var():
+    """Test that the above declared fastapi style endpoint works as per fastapi."""
+    client = new_client(vs4)
+    response = client.get("/items/1")
+    assert response.status_code == status.HTTP_200_OK, response.text
+    assert response.json() == {"type": "item", "id": 1}
---
+++ framework/src/brewing/http/tests/test_classbased.py
@@ -0,0 +1,73 @@
+"""Class based viewset tests"""
+
+from typing import Annotated
+from brewing.http import ViewSet, status, self
+from brewing.http.path import TrailingSlashPolicy
+from .helpers import SomeData
+from fastapi import APIRouter, Depends, HTTPException
+
+
+class ItemViewSet(ViewSet):
+    def __init__(
+        self,
+        root_path: str = "",
+        router: APIRouter | None = None,
+        trailing_slash_policy: TrailingSlashPolicy = TrailingSlashPolicy.default(),
+    ):
+        super().__init__(root_path, router, trailing_slash_policy)
+        # We make a rudimentary database being a simple dict
+        self._db: dict[int, SomeData] = {}
+        # And 2 more databases to track what has been deleted and replaced.
+        self._deleted: dict[int, list[SomeData]] = {}
+        self._replaced: dict[int, list[SomeData]] = {}
+
+    @self.GET()
+    def list_items(self) -> list[SomeData]:
+        """List all the items."""
+        return list(self._db.values())
+
+    @self.POST()
+    def create_item(self, item: SomeData) -> SomeData:
+        """Create an item."""
+        id = sorted(self._db.keys())[-1]  # Find next key
+        self._db[id] = item
+        return item
+
+    item_id = self("item_id")
+
+    @item_id.DEPENDS()
+    def item(self, item_id: int) -> SomeData:
+        try:
+            return self._db[item_id]
+        except KeyError as error:
+            raise HTTPException(
+                detail=f"no item found with {item_id=}",
+                status_code=status.HTTP_404_NOT_FOUND,
+            ) from error
+
+    @item_id.GET()
+    def get_item(self, item: Annotated[SomeData, Depends(item)]):
+        return item
+
+    @item_id.PUT()
+    def update_item(
+        self,
+        item_id: int,
+        current_item: Annotated[SomeData, Depends(item)],
+        item: SomeData,
+    ):
+        if not self._replaced.get(item_id):
+            self._replaced[item_id] = []
+        self._replaced[item_id].append(current_item)
+        self._db[item_id] = item
+        return item
+
+    @item_id.DELETE()
+    def delete_item(self, item_id: int, item: Annotated[SomeData, Depends(item)]):
+        if not self._deleted.get(item_id):
+            self._deleted[item_id] = []
+        self._deleted[item_id].append(item)
+        del self._db[item_id]
+
+
+vs6 = ItemViewSet()
