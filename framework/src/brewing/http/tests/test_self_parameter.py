165a2fa42605f5432f808d2050e697b9edbc0987
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-10-07 22:08:25 +1300

    feat: first take at an annotion-modifying module.

---
+++ framework/src/brewing/http/tests/test_self_parameter.py
@@ -0,0 +1,67 @@
+"""Test use of the first parameter.
+
+Handling this paramater is the basis of class based views
+but also can be used for functional views.
+"""
+
+from brewing.http.path import TrailingSlashPolicy
+from fastapi import APIRouter
+from .helpers import new_client
+
+from brewing.http import ViewSet, status
+
+
+## Setup - we'll construct a subclass of viewset.
+# we won't have any class-based endpoints, but
+# the subclass will have a couple of extra attributes.
+# the first parameter of a functional endpoint, if untyped
+# or typed as the tyype of the ViewSet, will be adapted via fastapi depends mechanism
+# to be passed.
+class CluckingViewSet(ViewSet):
+    def __init__(
+        self,
+        root_path: str = "",
+        router: APIRouter | None = None,
+        trailing_slash_policy: TrailingSlashPolicy = ...,
+    ):
+        super().__init__(root_path, router, trailing_slash_policy)
+        self.sound = "cluck"
+
+
+def test_untyped_first_parameter():
+    vs1 = CluckingViewSet()
+
+    @vs1.GET()
+    def make_sound(self, *, shout: bool = False) -> str:  # type: ignore
+        assert isinstance(self, CluckingViewSet)
+        sound: str = self.sound  # type: ignore
+        if shout:
+            sound = sound.upper()
+        return sound
+
+    client = new_client(vs1)
+    result = client.get("/")
+    assert result.status_code == status.HTTP_200_OK, result.text
+    assert result.text == "cluck"
+    result = client.get("/?shout=1")
+    assert result.status_code == status.HTTP_200_OK, result.text
+    assert result.text == "CLUCK"
+
+
+def test_viewset_typed_first_parameter():
+    vs1 = CluckingViewSet()
+
+    @vs1.GET()
+    def make_sound(self: CluckingViewSet, *, shout: bool = False) -> str:  # type: ignore
+        sound: str = self.sound
+        if shout:
+            sound = sound.upper()
+        return sound
+
+    client = new_client(vs1)
+    result = client.get("/")
+    assert result.status_code == status.HTTP_200_OK, result.text
+    assert result.text == "cluck"
+    result = client.get("/?shout=1")
+    assert result.status_code == status.HTTP_200_OK, result.text
+    assert result.text == "CLUCK"
