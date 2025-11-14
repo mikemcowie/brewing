580710c61ebee985827861a9d83baea136029c26
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-11-14 15:46:23 +1300

    test: Brewing class behaviour unit test.

--- framework/src/brewing/main_test.py
+++ framework/src/brewing/main_test.py
@@ -28,3 +28,38 @@ def test_brewing_options_global_loaded(subtests: SubTests):
         assert main.BrewingOptions.current() is options
     with subtests.test("raises-after-exit"), pytest.raises(main.NoCurrentOptions):
         assert main.BrewingOptions.current()
+
+
+# pyright: reportUnusedExpression=false
+
+
+def test_brewing(subtests: SubTests):
+    """Test the setup of the Brewing class."""
+    comp1 = MagicMock()
+    comp2 = MagicMock()
+    options = new_options()
+    with options:
+        app = main.Brewing(comp1=comp1, comp2=comp2)
+    with subtests.test("components-attribute"):
+        assert app.components == {
+            "comp1": comp1,
+            "comp2": comp2,
+            "db": options.database,
+        }
+    with subtests.test("components-registered"):
+        comp1.register.assert_called_once_with("comp1", app)
+        comp2.register.assert_called_once_with("comp2", app)
+        options.database.register.assert_called_once_with("db", app)  # type: ignore
+    with subtests.test("components-available-as-attributes"):
+        ## components are available as dynamic attributes.
+        # this allows an entrypoint module to use module-level __getattr__ to expose them.
+
+        app.comp1 is comp1
+        app.comp2 is comp2
+        app.db is options.database
+
+    with (
+        subtests.test("__getattr__ fails on arbitart attr"),
+        pytest.raises(AttributeError),
+    ):
+        app.foo_bar
