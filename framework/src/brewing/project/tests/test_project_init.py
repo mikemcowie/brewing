3c50ea0e34cd94b4d23a42bedf9f0c70806e2e59
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-11-16 23:52:34 +1300

    feat: project generator and CLI for managing the project.

---
+++ framework/src/brewing/project/tests/test_project_init.py
@@ -0,0 +1,128 @@
+"""Project init - start a new brewing project."""
+
+import os
+import sys
+import signal
+import uv
+import subprocess
+from typing import Callable, Any
+from contextlib import contextmanager
+import httpx
+from pathlib import Path
+import brewing
+from brewing.cli.testing import BrewingCLIRunner
+from brewing.project import cli
+from brewing.db import testing as db_testing
+from brewing.db.settings import DatabaseType
+from tenacity import retry, wait_exponential_jitter, stop_after_delay
+
+
+@contextmanager
+def cd(path: Path | str):
+    """Context manager to temporarily change working directory."""
+    cwd = os.getcwd()
+    os.chdir(str(path))
+    yield
+    os.chdir(cwd)
+
+
+@contextmanager
+def run(*cmd: str, readiness_callback: Callable[..., Any], cwd: Path | None = None):
+    """Run given command in a background thread."""
+    proc = subprocess.Popen(cmd, stdout=sys.stdout, stderr=sys.stderr, cwd=cwd)
+    readiness_callback()
+    yield
+    proc.send_signal(signal.SIGTERM)
+    proc.wait(5)
+    proc.kill()
+
+
+def cli_runner():
+    runner = BrewingCLIRunner(cli.load())
+    return runner
+
+
+def test_cli_commands_change_if_active_project_found(tmp_path: Path):
+    # Given an empty directory
+    project_dir = tmp_path / "my-project"
+    project_dir.mkdir()
+    with cd(project_dir):
+        # list the names of the CLI commands, init is there.
+        res = subprocess.run(
+            [uv.find_uv_bin(), "run", "brewing", "--help"],
+            cwd=project_dir,
+            capture_output=True,
+            encoding="utf8",
+        )
+        out = res.stderr + res.stdout
+        if res.returncode:
+            assert False, out
+        assert "project" in out
+        assert "my-project" not in out
+
+        # If I run init in the directory
+        runner = cli_runner()
+        runner.invoke(["init", "--path", str(project_dir)], catch_exceptions=False)
+        # Then the command names are different with the existing commands moved
+        out = runner.invoke(
+            ["init", "--path", str(project_dir)], catch_exceptions=False
+        )
+        assert "project" in out.output, out.output
+        assert "my-project" in out.output, out.output
+
+
+def test_project_init(tmp_path: Path):
+    """CLI allows project initialization."""
+    # Given an empty directory
+    project_dir = tmp_path / "my-project"
+    project_dir.mkdir()
+    # If I run init in the directory
+    runner = cli_runner()
+    runner.invoke(["init", "--path", str(project_dir)], catch_exceptions=False)
+    subprocess.run([uv.find_uv_bin(), "sync"], check=True, cwd=project_dir)
+    # Change install of brewing to local install
+    subprocess.run(
+        [
+            uv.find_uv_bin(),
+            "add",
+            str(
+                Path(brewing.__file__).parents[2].relative_to(project_dir, walk_up=True)
+            ),
+        ],
+        check=True,
+        cwd=project_dir,
+    )
+
+    # Then the directory will contain a starter brewing project
+    assert set(f for f in project_dir.glob("**") if ".venv" not in f.parts) == {
+        project_dir,
+        project_dir / "README.md",
+        project_dir / "pyproject.toml",
+        project_dir / "uv.lock",
+        project_dir / "src",
+        project_dir / "src" / "my_project",
+        project_dir / "src" / "my_project" / "__init__.py",
+        project_dir / "src" / "my_project" / "app.py",
+        project_dir / ".gitignore",
+    }
+
+    @retry(wait=wait_exponential_jitter(initial=0.1, max=2), stop=stop_after_delay(10))
+    def readiness_callback():
+        live_status = httpx.get("http://127.0.0.1:8000/livez")
+        live_status.raise_for_status()
+        ready_status = httpx.get("http://127.0.0.1:8000/readyz")
+        ready_status.raise_for_status()
+
+    # start the dev server (in another thread)
+    with (
+        db_testing.dev(DatabaseType.postgresql),
+        run(
+            "uv",
+            "run",
+            "brewing",
+            "http",
+            readiness_callback=readiness_callback,
+            cwd=project_dir,
+        ),
+    ):
+        pass  # The test is all in the contextmanager
