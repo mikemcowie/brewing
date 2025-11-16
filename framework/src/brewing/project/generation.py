2bb5ac5946723fd1fb73f3c36d11105f64236163
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-11-16 17:20:01 +1300

    test: new test covering subdirectories.

--- framework/src/brewing/project/generation.py
+++ framework/src/brewing/project/generation.py
@@ -31,7 +31,9 @@ class ManagedDirectory:
             if isinstance(file, NormalFile):
                 file.materialize_with(name, self.config)
                 continue
-            # Otherwise make a
+            # Otherwise make a directory and repeat
+            # on that directory.
+            path.mkdir(exist_ok=True, parents=True)
             subdir = self.__class__(files=file, config=replace(self.config, path=path))
             subdir.materialize()
 
@@ -48,7 +50,6 @@ class NormalFile:
 
     def materialize_with(self, name: str, config: ProjectConfiguration) -> None:
         """Materializes the file within the given directory."""
-        path = config.path / name
         if not config.path.is_absolute():
             raise MaterializationError(
                 "Cannot materialize a file with a relative directory"
@@ -57,32 +58,10 @@ class NormalFile:
         # by walking the tree up and checking everything is either a directory or doesn't exist.
         if not config.path.parents:
             raise ValueError("Cannot operate on the root file.")
-        dir = (
-            config.path.parent
-        )  # just assigning dir to prevent possibly unbound variable type error.
-        for dir in config.path.parents:
-            if dir.is_dir():
-                break
-            if dir.exists():
-                raise MaterializationError(
-                    f"cannot materialize {dir=!s} "
-                    f"because {dir=!s} exists and is not a directory."
-                )
-        path.write_text(
+        config.path.mkdir(exist_ok=True, parents=True)
+        (config.path / name).write_text(
             self.content if isinstance(self.content, str) else self.content(config)
         )
-
-
-@dataclass
-class MaterializedFile:
-    """Represents a file after its contents and path are computed in the project context."""
-
-    path: Path
-    content: str
-
-
-project = ManagedDirectory(files={}, config=ProjectConfiguration("foo", path=Path()))
-print(project)
 
 
 def test_project_materializes_files_in_immediate_directory(tmp_path: Path):
@@ -105,3 +84,53 @@ def test_project_materializes_files_in_immediate_directory(tmp_path: Path):
     # And have the expected content
     assert (tmp_path / "file1").read_text() == "foo"
     assert (tmp_path / "file2").read_text() == "bar"
+
+
+def test_project_materializes_subdirectories(tmp_path: Path):
+    """We can use nested dicts to describe a target state with subdirectories and materialize it."""
+    # Given a target state of several layers of files and directories.
+    files: Directory = {
+        "empty-dir": {},
+        "some-file0": NormalFile(content="some-file0-content"),
+        "dir1": {
+            "some-file1": NormalFile(content="some-file1-content"),
+            "dir2": {
+                "some-file2": NormalFile(content="some-file2-content"),
+                "dir3": {
+                    "some-file3": NormalFile(content="some-file3-content"),
+                },
+            },
+        },
+    }
+    project = ManagedDirectory(
+        files=files, config=ProjectConfiguration(name="test", path=tmp_path)
+    )
+    # If we materialize
+    project.materialize()
+    # Then we can check the filesystem matches what was expected.
+    assert set(tmp_path.glob("**")) == {
+        tmp_path,
+        tmp_path / "empty-dir",
+        tmp_path / "some-file0",
+        tmp_path / "dir1",
+        tmp_path / "dir1" / "some-file1",
+        tmp_path / "dir1" / "dir2",
+        tmp_path / "dir1" / "dir2" / "some-file2",
+        tmp_path / "dir1" / "dir2" / "dir3",
+        tmp_path / "dir1" / "dir2" / "dir3" / "some-file3",
+    }
+    # And we can check the relevent files are files versus directories
+    for file in tmp_path.glob("**"):
+        if "dir" in file.name:
+            assert file.is_dir()
+        elif "file" in file.name:
+            assert file.is_file()
+    # And check the file content
+    assert (tmp_path / "some-file0").read_text() == "some-file0-content"
+    assert (tmp_path / "dir1" / "some-file1").read_text() == "some-file1-content"
+    assert (
+        tmp_path / "dir1" / "dir2" / "some-file2"
+    ).read_text() == "some-file2-content"
+    assert (
+        tmp_path / "dir1" / "dir2" / "dir3" / "some-file3"
+    ).read_text() == "some-file3-content"
