e2cbf6620d2ee412ac183fbd5605272725c6e597
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-11-22 22:16:38 +1300

    refactor: move env helper to context.py.

--- src/brewing/context.py
+++ src/brewing/context.py
@@ -2,9 +2,13 @@
 
 from __future__ import annotations
 
+import os
 from contextlib import contextmanager
 from contextvars import ContextVar
-from typing import Any, ClassVar, Self
+from typing import TYPE_CHECKING, Any, ClassVar, Self
+
+if TYPE_CHECKING:
+    from collections.abc import Generator, MutableMapping
 
 
 class HasGlobalContext:
@@ -41,3 +45,22 @@ def push(*instance: HasGlobalContext):
         i.__class__.current.reset(token)
         for i, token in zip(instance, tokens, strict=True)
     ]
+
+
+@contextmanager
+def env(
+    new_env: dict[str, str], environ: MutableMapping[str, str] = os.environ
+) -> Generator[None]:
+    """Temporarily modify environment (or other provided mapping), restore original values on cleanup."""
+    orig: dict[str, str | None] = {}
+    for key, value in new_env.items():
+        orig[key] = environ.get(key)
+        environ[key] = value
+    yield
+    # Cleanup - restore the original values
+    # or delete if they weren't set.
+    for key, value in orig.items():
+        if value is None:
+            del environ[key]
+        else:
+            environ[key] = value
