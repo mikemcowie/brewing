8593a9bc79ee71355bf8d892f570828fd1f02682
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-11-23 18:05:23 +1300

    feat: be able to pass objects as environment variables

--- src/brewing/config.py
+++ src/brewing/config.py
@@ -3,9 +3,13 @@
 from __future__ import annotations
 
 import importlib
-from typing import Annotated, Any
+import os
+from contextlib import contextmanager
+from typing import Annotated, Any, get_type_hints
 
 from pydantic import BaseModel, BeforeValidator, ConfigDict, PlainSerializer
+
+from brewing.context import env
 
 
 def type_to_string(cls: type[Any]) -> str:
@@ -23,32 +27,75 @@ def string_to_type(value: type[Any] | str) -> type[Any]:
     return cls
 
 
-class BaseConfig(BaseModel):
+class BaseOptions(BaseModel):
     """Base model from which all configurations inherit.
 
     It holds information about the class it is a configuration for.
     """
 
-    model_config = ConfigDict(
-        # We can deserialize objects that were origonally subclasses to BaseConfig
-        # and they will work fine in a duck-type manner.
-        extra="allow"
-    )
+    # extra: allow allows the base class to initially be usued
+    # even when it's going to switch to a sub class subsequently.
+    model_config = ConfigDict(extra="allow")
+
+
+class EnvDTO[OptionsT: BaseOptions](BaseModel):
+    """Object to be transported via environment variable as JSON value."""
+
     cls: Annotated[
-        type[Any], PlainSerializer(type_to_string), BeforeValidator(string_to_type)
+        type[EnvPushable],
+        PlainSerializer(type_to_string),
+        BeforeValidator(string_to_type),
     ]
+    options_cls: Annotated[
+        type[OptionsT], PlainSerializer(type_to_string), BeforeValidator(string_to_type)
+    ]
+    options: OptionsT
 
 
-class Target:
-    pass
+class EnvPushableMeta(type):
+    """Metaclass where __instancecheck__  performs the following checks:
+
+    * object has an "options" value that us an instance of BaseConfig
+    """
+
+    def __instancecheck__(cls, instance: Any) -> bool:
+        return all(
+            (
+                hasattr(instance, "options"),
+                isinstance(instance.options, BaseOptions),
+                hasattr(instance, "__init__"),
+                list(get_type_hints(instance.__init__).keys()) == ["options"],
+                issubclass(
+                    next(iter(get_type_hints(instance.__init__).values())), BaseOptions
+                ),
+            )
+        )
 
 
-def test_type_serializer():
-    """Test we can declare a field with type type[T] 4
+class EnvPushable[OptionsT: BaseOptions](metaclass=EnvPushableMeta):
+    """Base class for objects that can be pushed and pulled through environment variables to subprocesses."""
 
-    It should be able to be serialized/deserialized through JSON."""
-    initial = BaseConfig(cls=Target)
-    json = initial.model_dump_json()
-    final = BaseConfig.model_validate_json(json)
-    assert final.cls is Target
-    assert initial == final
+    def __init__(self, options: OptionsT) -> None:
+        self.options = options
+
+
+@contextmanager
+def push_to_env(obj: EnvPushable[Any], env_var: str):
+    with env(
+        {
+            env_var: EnvDTO(
+                cls=obj.__class__,
+                options=obj.options,
+                options_cls=obj.options.__class__,
+            ).model_dump_json()
+        }
+    ):
+        yield
+
+
+def pull_from_env[T: EnvPushable[Any]](env_var: str, check_type: type[T]) -> T:
+    dto: EnvDTO[Any] = EnvDTO.model_validate_json(os.environ[env_var])
+    retval = dto.cls(options=dto.options_cls(**dto.options.model_dump()))
+    if not isinstance(retval, check_type):
+        raise TypeError(f"{retval} is not instance of {check_type}")
+    return retval
--- src/brewing/config_test.py
+++ src/brewing/config_test.py
@@ -5,25 +5,20 @@ from __future__ import annotations
 from brewing import config as c
 
 
-class Target:
+class TargetOptions(c.BaseOptions):
+    name: str
+
+
+class Target(c.EnvPushable[TargetOptions]):
     pass
-
-
-def test_type_serializer():
-    """Test we can declare a field with type type[T] 4
-
-    It should be able to be serialized/deserialized through JSON."""
-    initial = c.BaseConfig(cls=Target)
-    json = initial.model_dump_json()
-    final = c.BaseConfig.model_validate_json(json)
-    assert final.cls is Target
-    assert initial == final
 
 
 def test_type_serializer_transmitted_by_environment():
     """Affirm we can use environment variables to transmit the object"""
-    initial = c.BaseConfig(cls=Target)
-    with c.push_to_env(initial):
-        final = pull_from_env(Target)
-    assert final.cls is Target
-    assert initial == final
+    target = Target(TargetOptions(name="something"))
+    env_var = "SOME-VAR"
+    with c.push_to_env(target, env_var):
+        final = c.pull_from_env(env_var, Target)
+    assert type(final) is type(target)
+    assert final.options == target.options
+    assert final.__dict__ == target.__dict__
