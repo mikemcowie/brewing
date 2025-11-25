19863cce5703a34b23143351f1e4f43912999b30
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-11-25 23:27:02 +1300

    refactor: BrewingHTTP converted to a dataclass that will be easier to serialize.

--- src/brewing/http/asgi.py
+++ src/brewing/http/asgi.py
@@ -7,6 +7,7 @@ It is a shallow wrapper around fastapi with extra methods to support native feat
 from __future__ import annotations
 
 import inspect
+from dataclasses import asdict, dataclass
 from typing import TYPE_CHECKING, Annotated, Any, Self
 
 import uvicorn
@@ -16,6 +17,8 @@ from typer import Option
 from brewing.db import testing
 
 if TYPE_CHECKING:
+    from starlette.types import Receive, Scope, Send
+
     from brewing import Brewing
 
     from . import ViewSet
@@ -28,28 +31,43 @@ def find_calling_module():
         assert frame
         frame = frame.f_back
         module = inspect.getmodule(frame)
-        assert module
+        if not module:
+            continue
         mod_name = module.__name__
         if mod_name != __name__:
             return mod_name
 
 
-class BrewingHTTP(FastAPI):
+@dataclass
+class BrewingHTTP:
     """
-    The brewing ASGI application.
+    The brewing application.
 
-    It is subclassed from FastAPI with extra methods to handle and translate
+    It is is a facade around a FastAPI instance with extra methods to handle and translate
     brewing-specific objects.
     """
 
-    app_string_identifier: str
-    if not TYPE_CHECKING:
+    title: str = "A Brewing API"
+    description: str | None = None
+    summary: str | None = None
+    version: str = "0.1.0"
+    openapi_url: str | None = "/openapi.json"
+    docs_url: str | None = "/docs"
+    redoc_url: str | None = "/redoc"
 
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self.app_string_identifier = (
-                f"{find_calling_module()}:{self.extra.get('name', 'http')}"
-            )
+    def __post_init__(self):
+        self._fastapi = FastAPI(**asdict(self))
+        self.app_string_identifier = (
+            f"{find_calling_module()}:{self._fastapi.extra.get('name', 'http')}"
+        )
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        """Call fastapi instance.
+
+        This method allows the brewing HTTP instance itself to act as an
+        ASGI application and hence a direct targetf for webserver like uvicorn.
+        """
+        return await self._fastapi(scope, receive, send)
 
     def register(self, name: str, brewing: Brewing, /):
         """Register http server to brewing."""
@@ -87,7 +105,7 @@ class BrewingHTTP(FastAPI):
             **kwargs (Any): passed directly to FastAPI.include_router
 
         """
-        self.include_router(viewset.router, **kwargs)
+        self._fastapi.include_router(viewset.router, **kwargs)
 
     def with_viewsets(self, *vs: ViewSet[Any]) -> Self:
         """
@@ -103,3 +121,6 @@ class BrewingHTTP(FastAPI):
         for v in vs:
             self.include_viewset(v)
         return self
+
+
+BrewingHTTP(title="foo")
