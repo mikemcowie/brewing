c985ccd8ae00c830478672b2975feba5e6ad48e2
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-12-05 22:05:40 +1300

    feat: initial content negotiation implementation.

--- src/brewing/http/__init__.py
+++ src/brewing/http/__init__.py
@@ -1,9 +1,10 @@
 """An http toolkit built on fastapi."""
 
+from fastapi import HTTPException
 from fastapi import status as status
 
 from brewing.http.asgi import BrewingHTTP as BrewingHTTP
 from brewing.http.path import self
 from brewing.http.viewset import ViewSet as ViewSet
 
-__all__ = ["BrewingHTTP", "ViewSet", "self", "status"]
+__all__ = ["BrewingHTTP", "HTTPException", "ViewSet", "self", "status"]
