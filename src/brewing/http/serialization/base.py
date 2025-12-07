26a25a8bd302b136e94b2258ae84ac3c502bf898
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-12-07 20:03:58 +1300

    refactor: convert Renderer and Loader to ABCs instead of protocols so we will be able to use isinstance at runtime.

--- src/brewing/http/serialization/base.py
+++ src/brewing/http/serialization/base.py
@@ -6,39 +6,14 @@ the options availanble at endpoint functions.
 
 from __future__ import annotations
 
-from abc import abstractmethod
-from dataclasses import dataclass
-from typing import TYPE_CHECKING, ClassVar, Protocol, Self
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING, ClassVar, Self, final
 
 if TYPE_CHECKING:
-    from collections.abc import Iterable, Iterator
-
-    from brewing.http.serialization.negotiation import Negotiator
+    from collections.abc import Iterator
 
 
-@dataclass
-class Processors[InputT, InternalT, OutputT]:
-    """Hold the loader and renderer(s) for each function."""
-
-    negotiator: Negotiator
-    loader: Loader[InputT, InternalT]
-    renderer: Iterable[Renderer[InternalT, OutputT]]
-
-    def __post_init__(self):
-        if len(set(self.renderer)) < len(list(self.renderer)):
-            raise RuntimeError(
-                "Invalid renderers: 2 were provided with same content type.",
-                {"renderers": self.renderer},
-            )
-
-    def load(self, obj: InputT, /) -> InternalT:
-        return self.loader(obj)
-
-    def render(self, obj: InternalT, accepts: str, /) -> OutputT:
-        return self.negotiator.select(accepts, self.renderer)(obj)
-
-
-class Loader[InputT, InternalT](Protocol):
+class Loader[InputT, InternalT](ABC):
     """Convert an object from the form fastapi loaded from the request, into the form annotated on the endpoint."""
 
     @abstractmethod
@@ -47,13 +22,20 @@ class Loader[InputT, InternalT](Protocol):
         ...
 
 
-class Renderer[InternalT, OutputT](Protocol):
+class Renderer[InternalT, OutputT](ABC):
     """Convert the application's internal representation of a resource to the form fastapi will return."""
 
     content_type: ClassVar[str]
 
+    @final
     def __iter__(self) -> Iterator[Self]:
-        """Iterate over the renderer, yielding self."""
+        """Iterate over the renderer, yielding self.
+
+        Being an iterable yielding itself allows us to simplify
+        type hinting and runtime logic, as we're allowed to  pass
+        exactly one renderer and it still can be iterated over just
+        like a Seequence of renderers.
+        """
         return iter((self,))
 
     @abstractmethod
