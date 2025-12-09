2e165d08eae9b2a5e789168eceacacda6af13f0a
Mike Cowie <24739590+mikemcowie@users.noreply.github.com>
2025-12-09 23:55:03 +1300

    refactor: break __init__ into parts

--- src/brewing/http/serialization/loaders.py
+++ src/brewing/http/serialization/loaders.py
@@ -25,61 +25,49 @@ class Loader[InputT, InternalT](ABC):
 
 
 class TypeLoader[InternalT](Loader[BaseModel, InternalT]):
-    def __init__(self, internal_t: Callable[..., InternalT]) -> None:
-        if isinstance(internal_t, type):
-            self._internal_t = internal_t
-            self._factory = internal_t.__init__
-            if not isinstance(self._factory, FunctionType):
-                raise TypeError(
-                    f"class {internal_t} does not have an __init__ method and hence cannot be used here."
-                )
-        elif isinstance(internal_t, FunctionType):
-            try:
-                return_annotation = get_type_hints(self._internal_t)["return"]
-            except KeyError as error:
-                raise TypeError(
-                    f"function {internal_t} does not have a return annotation and hence cannot be used in this context."
-                ) from error
-            if not isinstance(return_annotation, type):
-                raise TypeError(
-                    f"return annotation of {self._internal_t}, {return_annotation} is not a type"
-                )
-            self._internal_t = return_annotation
-        else:
+    @staticmethod
+    def _load_from_type(internal_t: type):
+        _internal_t = internal_t
+        _factory = internal_t.__init__
+        if not isinstance(_factory, FunctionType):
             raise TypeError(
-                f"{internal_t} is neither a class nor a function and hence cannot be used in this context."
+                f"class {internal_t} does not have an __init__ method and hence cannot be used here."
             )
-        self._signature = inspect.signature(self._factory)
+        return _internal_t, _factory
 
-        self._type_hints = get_type_hints(self._factory)
-        if (
-            issubclass(self._internal_t, DeclarativeBase)
-            and self._factory is self._internal_t.__init__
-            and self._factory is not DeclarativeBase.__init__
-        ):
-            # sqlalchemy creates a wrapping __init__ method, we can only access the attributes of the original factory
-            # by inspecting an internal attibute.
-            self._type_hints = get_type_hints(
-                self._internal_t.__init__._sa_original_init  # noqa: SLF001 # type: ignore
+    @staticmethod
+    def _load_from_callable(internal_t: FunctionType):
+        try:
+            return_annotation = get_type_hints(internal_t)["return"]
+        except KeyError as error:
+            raise TypeError(
+                f"function {internal_t} does not have a return annotation and hence cannot be used in this context."
+            ) from error
+        if not isinstance(return_annotation, type):
+            raise TypeError(
+                f"return annotation of {internal_t}, {return_annotation} is not a type"
             )
+        return return_annotation, internal_t
 
-        self._untyped_params = set(self._signature.parameters.keys()).difference(
+    def _validate_untyped_params(self):
+        _untyped_params = set(self._signature.parameters.keys()).difference(
             self._type_hints
         )
-        if self._untyped_params:
-            # The first paramter if allowed to be untyped, to allow for 'self' / 'cls' parameters
-            # which conventionally are not type hinted.
-            first_parameter = next(iter(self._signature.parameters.keys()))
-            if first_parameter in self._untyped_params:
-                self._untyped_params.remove(first_parameter)
-            if self._untyped_params:
-                raise TypeError(
-                    f"Missing type parameters for {self._factory.__module__}:{self._factory.__qualname__}: {self._untyped_params}"
-                )
         parameter_kinds = {
             name: parameter.kind
             for name, parameter in list(self._signature.parameters.items())[1:]
         }
+
+        if _untyped_params:
+            # The first paramter if allowed to be untyped, to allow for 'self' / 'cls' parameters
+            # which conventionally are not type hinted.
+            first_parameter = next(iter(self._signature.parameters.keys()))
+            if first_parameter in _untyped_params:
+                _untyped_params.remove(first_parameter)
+        if _untyped_params:
+            raise TypeError(
+                f"Missing type parameters for {self._factory.__module__}:{self._factory.__qualname__}: {_untyped_params}"
+            )
         if (
             inspect.Parameter.POSITIONAL_ONLY in parameter_kinds.values()
             or inspect.Parameter.VAR_POSITIONAL in parameter_kinds.values()
@@ -90,6 +78,38 @@ class TypeLoader[InternalT](Loader[BaseModel, InternalT]):
                 "function must accept at least 1 named keyword argument"
                 " (not **kwargs) in  __init__."
             )
+
+    def _load_type_hints(self):
+        if (
+            issubclass(self._internal_t, DeclarativeBase)
+            and self._factory is self._internal_t.__init__
+            and self._factory is not DeclarativeBase.__init__
+        ):
+            # sqlalchemy creates a wrapping __init__ method, we can only access the attributes of the original factory
+            # by inspecting an internal attibute.
+            return get_type_hints(
+                self._internal_t.__init__._sa_original_init  # noqa: SLF001 # type: ignore
+            )
+        else:
+            return get_type_hints(self._factory)
+
+    def _load_type_and_factory(
+        self, internal_t: Callable[..., InternalT]
+    ) -> tuple[type, FunctionType]:
+        if isinstance(internal_t, type):
+            return self._load_from_type(internal_t)
+        elif isinstance(internal_t, FunctionType):
+            return self._load_from_callable(internal_t)
+        else:
+            raise TypeError(
+                f"{internal_t} is neither a class nor a function and hence cannot be used in this context."
+            )
+
+    def __init__(self, internal_t: Callable[..., InternalT]) -> None:
+        self._internal_t, self._factory = self._load_type_and_factory(internal_t)
+        self._signature = inspect.signature(self._factory)
+        self._type_hints = self._load_type_hints()
+        self._validate_untyped_params()
 
     @cached_property
     def model(self) -> type[BaseModel]:
